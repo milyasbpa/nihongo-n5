9ac125a085944e212124f88ba834b40c702c2cfd
milyasbpa <basilyas.2015@gmail.com>
2025-04-16 14:08:27 +0700

    fix: error build

--- src/features/vocabulary/container/Vocabulary.container.tsx
+++ src/features/vocabulary/container/Vocabulary.container.tsx
@@ -6,15 +6,17 @@ import { AnswerVocabulary } from "../fragments/answer";
 
 export const VocabularyContainer = () => {
   return (
-    <div
-      className={clsx(
-        "grid grid-cols-1 place-content-start place-items-start gap-[1rem]",
-        "w-full"
-      )}
-    >
-      <ProgressVocabulary />
-      <QuestionVocabulary />
-      <AnswerVocabulary />
-    </div>
+    <React.Suspense>
+      <div
+        className={clsx(
+          "grid grid-cols-1 place-content-start place-items-start gap-[1rem]",
+          "w-full"
+        )}
+      >
+        <ProgressVocabulary />
+        <QuestionVocabulary />
+        <AnswerVocabulary />
+      </div>
+    </React.Suspense>
   );
 };
