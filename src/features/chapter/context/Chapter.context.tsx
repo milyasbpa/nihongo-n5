dd559eb8e535668b215a5563d6edab1284f7bf66
milyasbpa <basilyas.2015@gmail.com>
2025-05-02 19:29:24 +0700

    fix: error

--- src/features/chapter/context/Chapter.context.tsx
+++ src/features/chapter/context/Chapter.context.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { createContext, useReducer, Dispatch, useEffect } from "react";
+import React, { createContext, useReducer, Dispatch } from "react";
 import { ChapterActions, ChapterInitialStateType } from "./Chapter.types";
 import {
   ChapterKanjiReducers,
--- src/features/chapter/react_query/hooks/index.tsx
+++ src/features/chapter/react_query/hooks/index.tsx
@@ -1,2 +1,1 @@
-export * from "./useGetVocabularyCategoryList.chapter";
 export * from "./useGetKanjiCategoryList.chapter";
